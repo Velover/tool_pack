c2c538ec54c21f32eeba89240e1d88ee7d7341ee
Catargo <cobauper@gmail.com>
2024-06-20 13:45:00 +0200

    changed imports and tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 export * from "./classes"
 
-export { ArrayTools } from "./array_tools";
-export { AssetsTools } from "./assets_tools";
-export { ColorTools } from "./color_tools";
-export { DebuggingTools } from "./debugging_tools";
-export { DetectingTools } from "./detecting_tools";
-export { FunctionTools } from "./function_tools";
-export { HttpTools } from "./http_tools";
-export { InstanceTools } from "./instance_tools";
-export { MathTools } from "./math_tools";
-export { NetworkingTools } from "./networking_tools";
-export { PhysicsTools } from "./physics_tools";
-export { StringTools } from "./string_tools";
-export { TableTools } from "./table_tools";
-export { TweenTools } from "./tween_tools";
-export { Vector2Tools } from "./vector2_tools";
-export { Vector3Tools } from "./vector3_tools";
+export * from "./array_tools";
+export * from "./assets_tools";
+export * from "./color_tools";
+export * from "./debugging_tools";
+export * from "./detecting_tools";
+export * from "./function_tools";
+export * from "./http_tools";
+export * from "./instance_tools";
+export * from "./math_tools";
+export * from "./networking_tools";
+export * from "./physics_tools";
+export * from "./string_tools";
+export * from "./table_tools";
+export * from "./tween_tools";
+export * from "./vector2_tools";
+export * from "./vector3_tools";
